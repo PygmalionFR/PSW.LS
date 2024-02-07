998a018892440f017498689f45eb4e6441b4fc11
PygmalionFR <ludovic.streetz@outlook.fr>
2024-02-07 11:51:55 +0100

    maj du responsive

--- index.php
+++ index.php
@@ -9,23 +9,20 @@
     <script src="https://kit.fontawesome.com/ba95712233.js" crossorigin="anonymous"></script>
 </head>
 <body>
-<div class="cursor-background"></div>
-    <div class="cursor-shadow"></div>
     <header>
         <h1>Ludovic Streetz</h1>
         <div class="minidescription">
             <h2>Formation</h2>
             <h3>Développeur Intégrateur Web</h3>
             <p>
-                Contrat d'apprentissage - Rythme : 3 semaines en entreprise / 1 semaine enécole
+                Contrat d'apprentissage - Rythme : 3 semaines en entreprise / 1 semaine en école
                 Formation éligible à l'Aide Exceptionnelle de 6000€
             </p>
-
         </div>
         <nav>
-            <a href="?page=default">A propos -</a><br><br>
-            <a href="?page=projet">Projet -</a><br><br>
-            <a href="?page=contact">Contact -</a><br><br>
+            <a href="?page=default">A propos </a><br><br>
+            <a href="?page=projet">Projet </a><br><br>
+            <a href="?page=contact">Contact </a><br><br>
         </nav>
         <div class="icon">
             <a href="https://github.com/PygmalionFR" target="_blank"><i class="fa-brands fa-github"></i></a>
@@ -52,13 +49,13 @@
         } else {
             include('page/propos.php');
         }
-        ?>
+    ?>
     </main>
     <script>
         document.addEventListener("DOMContentLoaded", function() {
             var pageContainer = document.querySelector('.page-container');
             pageContainer.style.transform = 'translateX(0)';
         });
-</script>
+    </script>
 </body>
 </html>
--- page/propos.php
+++ page/propos.php
@@ -9,6 +9,7 @@
         $age--;
     }
 ?>
+<h2 class="propo">Des infos sur moi</h2>
 <p class="propo">
     Je m'appelle Ludovic Streetz, passionné de développement âgé de <?php echo $age; ?> ans. <br>Mon parcours a débuté de manière autodidacte grâce à des ressources telles qu'OpenClassroom et des tutoriels sur YouTube. <br>
     Aujourd'hui, je suis à l'aise les langages fondamentaux tels que HTML, CSS et PHP. Mon apprentissage ne s'arrête pas là ; actuellement, je suis en train d'approfondir mes connaissances en SQL et Python. <br>
